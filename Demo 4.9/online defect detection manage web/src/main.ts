6f78e93e8a0be0dbe10806c85424ce4c2b51e1a9
dlwlrmaer <912357231@qq.com>
2024-04-09 12:33:49 +0800

    first commit

---
+++ Demo 4.9/online defect detection manage web/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+//导入路由器
+import router from './router'
+//导入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+createApp(App)
+.use(ElementPlus)
+.use(router)
+.mount('#app')
